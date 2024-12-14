4a9330a315c457ff69203d4c286607da46284f1f
uwuvern <uwuvern@outlook.com>
2024-12-14 10:30:57 +0200

    Updated Copyright & rebased

--- composeApp/src/commonMain/kotlin/kt/atlas/navigation/routable/composable/CompositionStage.kt
+++ composeApp/src/commonMain/kotlin/kt/atlas/navigation/routable/composable/CompositionStage.kt
@@ -1,3 +1,11 @@
+/*
+ * Copyright (c) 2024. Ashley <ax-ie>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ */
+
 package kt.atlas.navigation.routable.composable
 
 enum class CompositionStage {
